a8afa7036b73a642b54594d29008c719cae08e58
Carlos <carlosdiazgirol@hotmail.com>
2024-03-27 11:03:30 +0100

    ready to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
-// App.js
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Calculator from './Calculator';
-import Result from './Result';
-import { CalculatorProvider } from './CalculatorProvider';
 import './App.css';
 
 function App() {
   return (
-    <CalculatorProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Calculator />} />
-          <Route path="/resultado" element={<Result />} />
-        </Routes>
-      </Router>
-    </CalculatorProvider>
+    <></>
   );
 }
 
